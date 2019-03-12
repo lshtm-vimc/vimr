3704eaf47db13e855e4111778f8a08da39bc1355
Arminder Deol <arminderdeol@gmail.com>
2019-03-12 14:17:36 +0000

    changed file type

--- R/convertingdatafiles.R
+++ R/convertingdatafiles.R
@@ -1,6 +1,6 @@
 library(rio)
 
-setwd("/Users/AD/Dropbox/MEEV/2. UNIVAC/1. Recoding to R/univacr")
-age.specific.fert<- import("/Users/AD/Downloads/201810synthetic-2_dds-201710_as_fert_both.csv")
-export(age.specific.fert, "age.specific.fert.rds")
+setwd("/Users/AD/Dropbox/MEEV/2. UNIVAC/1. Recoding to R/univacr/data")
+data.agespecificfert<- import("/Users/AD/Downloads/201810synthetic-2_dds-201710_as_fert_both.csv")
+export(data.agespecificfert, "data.agespecificfert.rds")
 
