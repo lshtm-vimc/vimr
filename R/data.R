58cc184064a3266190a167003d2bc9a4763d5895
Arminder Deol <arminderdeol@gmail.com>
2019-03-14 11:21:07 +0000

    Updating data documentation

---
+++ R/data.R
@@ -0,0 +1,19 @@
+# Data.R - This file includes documentation of datasets.
+
+#' 1-year prevalence of cervical cancer
+#'
+#' A dataset containing the 1-year prevalence (proportion) of cervical cancer in 185 countries, as reported by
+#'     IARC's Globocan 2018 database.
+#'
+#' As per IARC definition -- The (1-year) prevalence of a given cancer
+#'     is the number of individuals within a defined population who have been diagnosed with that
+#'     cancer (within 1 year) and who are still alive at a given point in time (i.e. the survivors).
+#'
+#' @format A data table containing 185 observations of 103 variables.
+#' \describe{
+#'   \item{Country}{Country name}
+#'   \item{0..100}{Age 0-100}
+#'   \item{iso3}{ISO3 country code}
+#' }
+#' @source \url{https://gco.iarc.fr/today/online-analysis-table}
+"test_to_delete"
