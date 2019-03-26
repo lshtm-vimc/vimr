fc6cf372981b67922e1f4ea4651c2824a7084b6c
Kaja A <kaja_a@yahoo.com>
2019-03-26 10:35:44 +0000

    change package name to vimr from univacr

--- R/package.R
+++ R/package.R
@@ -1,18 +1,23 @@
 # package.R - This file includes introductory information of package.
 
-#' univacr: UNIVAC decision support model
+#' vimr: Vaccine Impact Model
 #'
-#' A universal framework for evaluating vaccine policy options in low- and
-#' middle-income countries. This R package is based of the spreadsheet-based
-#' tool that is accessible at https://www.paho.org/provac-toolkit/.
+#' "vimr" is a vaccine impact model (R package) for estimating the health impact
+#' of vaccination programmes at the national level for a given set of countries.
+#' This R package is based of the spreadsheet-based tool (UNIVAC) which is a
+#' universal framework for evaluating vaccine policy options in low- and
+#' middle-income countries, and is accessible at
+#' https://www.paho.org/provac-toolkit/,
+#'
 #'
 #' @section Vaccine impact model:
 #' \describe{
-#'   \item{Estimate the health impact and cost-effectiveness of vaccination.}
+#'   \item{Estimate the health impact of vaccination at the national level.}
 #'   \item{Hib, pneumococcal and rotavirus vacccination can be separately analysed.}
+#'   \item{Multiple countries can be analysed and results are generated for each country.}
 #' }
 #'
 #' @docType package
-#' @name univacr
+#' @name vimr
 NULL
 
